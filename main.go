8ab048db9b5d966513bedfb14de2b5c4ca49cbdb
Mehran <Mehran.moradii@outlook.com>
2024-11-17 08:00:31 +0330

    Add main.go

--- main.go
+++ main.go
@@ -4,27 +4,47 @@ import (
 	"fmt"
 )
 
-const tickets int = 50
+const totalTickets int = 50
 
 var confrenceName string = "Go-lang"
 var remainingTickets int = 50
-var userName string
+var firstName string
+var lastName string
+var email string
 var ticketNumber int
 
 func main() {
 	fmt.Printf("Welcome to %v with available tickets: %v \n", confrenceName, remainingTickets)
 
-	fmt.Println("Please Enter Your name: ")
-	fmt.Scan(&userName)
-
+	fmt.Println("Please Enter Your First Name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Please Enter Your Last Name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Please Enter Your Email: ")
+	fmt.Scan(&email)
 	fmt.Println("Please Enter Your tickets: ")
 	fmt.Scan(&ticketNumber)
 
-	userHandeler(userName, ticketNumber)
+	userHandeler(firstName, lastName, email, ticketNumber)
+	remainingTickets = totalTickets - ticketNumber
+	fmt.Printf("Total remaining tickets are %v \n", remainingTickets)
 
 }
 
-func userHandeler(userName string, ticketNumber int) (string, int) {
-	fmt.Printf("User %v booked %v tickets \n", userName, ticketNumber)
-	return userName, ticketNumber
+func userHandeler(firstName string, lastName string, email string, ticketNumber int) (string, string, string, int) {
+	fmt.Printf("User %v %v with email %v booked %v tickets \n", firstName, lastName, email, ticketNumber)
+	return firstName, lastName, email, ticketNumber
 }
+
+// func userFetchData () (string, string, string, int) {
+// 	fmt.Println("Please Enter Your First Name: ")
+// 	fmt.Scan(&firstName)
+// 	fmt.Println("Please Enter Your Last Name: ")
+// 	fmt.Scan(&lastName)
+// 	fmt.Println("Please Enter Your Email: ")
+// 	fmt.Scan(&email)
+// 	fmt.Println("Please Enter Your tickets: ")
+// 	fmt.Scan(&ticketNumber)
+
+// 	return firstName, lastName, email, ticketNumber
+// }
