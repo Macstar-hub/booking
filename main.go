06017b67718ad8ddc87a9dab651962ee98b11387
Mehran <Mehran.moradii@outlook.com>
2024-11-20 20:44:16 +0330

    Add user info function.

--- main.go
+++ main.go
@@ -14,25 +14,23 @@ var email string
 var ticketNumber int
 var allAvailableTickets int
 
-func main() {
+func inputUserInfo() (string, string, string, int) {
 	fmt.Printf("Welcome to %v with available tickets: %v \n", confrenceName, remainingTickets)
-
 	fmt.Println("Please Enter Your First Name: ")
 	fmt.Scan(&firstName)
 	fmt.Println("Please Enter Your Last Name: ")
 	fmt.Scan(&lastName)
 	fmt.Println("Please Enter Your Email: ")
 	fmt.Scan(&email)
-	fmt.Println("Please Enter Your tickets: ")
+	fmt.Println("Please Enter Your Tickets For Bokking: ")
 	fmt.Scan(&ticketNumber)
-	userHandeler(firstName, lastName, email, ticketNumber)
-	fmt.Printf("All booking users: %v \n", makeArray(firstName, lastName))
-	fmt.Printf("Total remaining tickets are %v \n", availableTickets(remainingTickets, ticketNumber))
 
+	return firstName, lastName, email, ticketNumber
 }
 
 func userHandeler(firstName string, lastName string, email string, ticketNumber int) (string, string, string, int) {
-	fmt.Printf("User %v %v with email %v booked %v tickets \n", firstName, lastName, email, ticketNumber)
+	userFirstName, userLastName, userEmail, userTicketNumber := inputUserInfo()
+	fmt.Printf("User %v %v with email %v booked %v tickets \n", userFirstName, userLastName, userEmail, userTicketNumber)
 	return firstName, lastName, email, ticketNumber
 }
 
@@ -45,4 +43,12 @@ func makeArray(firstName string, lastName string) []string {
 func availableTickets(remainingTickets int, ticketNumber int) int {
 	allAvailableTickets = remainingTickets - ticketNumber
 	return allAvailableTickets
+}
+
+func main() {
+	fmt.Printf("Welcome to %v with available tickets: %v \n", confrenceName, remainingTickets)
+	userHandeler(firstName, lastName, email, ticketNumber)
+	fmt.Printf("All booking users: %v \n", makeArray(firstName, lastName))
+	fmt.Printf("Total remaining tickets are %v \n", availableTickets(remainingTickets, ticketNumber))
+
 }
