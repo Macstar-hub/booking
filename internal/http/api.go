9a4dd5530a9ce66f7960e4ceb405ca8a1a70fbc5
Mehran <Mehran.moradii@outlook.com>
2024-12-11 16:41:11 +0330

    Add user info post method

--- internal/http/api.go
+++ internal/http/api.go
@@ -5,15 +5,33 @@ import (
 
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-var response = "Hello world"
+type userInfoTemplate struct {
+	firstName    string
+	lastName     string
+	email        string
+	ticketNumber int
+}
 
-func firstNamePost(firstName *gin.Context) {
-	firstNames := firstName.PostForm("firstnames")
-	fmt.Println("FirstName get from post: ", firstNames)
+func unserInfoPost(body *gin.Context) {
+	firstName := body.PostForm("firstnames")
+	lastName := body.PostForm("lastname")
+	email := body.PostForm("email")
+	ticketNumberString := body.PostForm("ticketnumber")
+
+	ticketNumber, _ := strconv.Atoi(ticketNumberString)
+	userInfo := userInfoTemplate{
+		firstName:    firstName,
+		lastName:     lastName,
+		email:        email,
+		ticketNumber: ticketNumber, //ticketNumber,
+	}
+
+	fmt.Println("FirstName get from post: ", userInfo.firstName, userInfo.lastName, userInfo.email, userInfo.ticketNumber)
 	//  curl -vvvv -XPOST  -d "firstnames=mamad" http://localhost/firstname
 	// while true; do curl -XPOST  -d "firstnames=mamad" http://localhost/firstname && sleep 0.2 && clear ;done
 }
@@ -24,7 +42,7 @@ func main() {
 		test.String(http.StatusOK, "Hello world from string.")
 	})
 
-	server.POST("/firstname", firstNamePost)
+	server.POST("/firstname", unserInfoPost)
 
 	server.GET("/", func(r *gin.Context) {
 		r.JSON(200, gin.H{
