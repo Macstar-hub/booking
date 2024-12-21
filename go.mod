04ab31dcd9f1c8cc87712d62d8ba83b6ce073836
Mehran <Mehran.moradii@outlook.com>
2024-12-21 23:54:07 +0330

    Add test keycloak function. but still i don't know how can work it ???? :)

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module booking
 go 1.23.3
 
 require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/streadway/amqp v1.1.0
+	golang.org/x/oauth2 v0.24.0
 )
 
 require (
@@ -27,6 +29,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pquerna/cachecontrol v0.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
@@ -35,5 +38,6 @@ require (
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/keycloak/keycloak.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	configURL := "http://localhost:8090/realms/admin-area"
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, configURL)
+	if err != nil {
+		log.Println("Cannot connection to keycloak: ", err)
+	}
+
+	clientID := "admin-booking"
+	clientSecret := "LOmb3mUS4YiRWlT6s9be7Uk3NHdSGBYU"
+
+	redirectURL := "http://localhost:8091/booking/callback"
+
+	ouath2Config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Endpoint:     provider.Endpoint(),
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+
+	state := "someState"
+
+	oidcConfig := &oidc.Config{
+		ClientID: clientID,
+	}
+
+	verifier := provider.Verifier(oidcConfig)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rawAccessToken := r.Header.Get("Authorization")
+		if rawAccessToken == "" {
+			http.Redirect(w, r, ouath2Config.AuthCodeURL(state), http.StatusFound)
+			return
+		}
+		parts := strings.Split(rawAccessToken, " ")
+		if len(parts) != 2 {
+			w.WriteHeader(400)
+			return
+		}
+		_, err := verifier.Verify(ctx, parts[1])
+		if err != nil {
+			http.Redirect(w, r, ouath2Config.AuthCodeURL(state), http.StatusFound)
+			return
+		}
+
+		w.Write([]byte("Hello from pervios token get."))
+	})
+
+	http.HandleFunc("/booking/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			http.Error(w, "State did not match", http.StatusBadRequest)
+		}
+		oauth2Token, err := ouath2Config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			fmt.Println("Cannot exchange oauth2.", err)
+		}
+		rawIDToken, _ := oauth2Token.Extra("id_token").(string)
+		idToken, err := verifier.Verify(ctx, rawIDToken)
+		if err != nil {
+			fmt.Println("Cannot verify token: ", err)
+		}
+
+		resp := struct {
+			OAuth2Token   *oauth2.Token
+			IDTokenClaims *json.RawMessage
+		}{oauth2Token, new(json.RawMessage)}
+
+		if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		data, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Write(data)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8091", nil))
+}
